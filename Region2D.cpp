2f73590f2233d5f5a972639e84838d9ccda2c4f1
codysauermann <80296561+codysauermann@users.noreply.github.com>
2023-03-07 19:09:07 -0500

    Started adding sweep for flag setting in Region

--- Region2D.cpp
+++ Region2D.cpp
@@ -18,12 +18,9 @@ struct Region2D::Impl {
     std::vector<AttributedHalfSegment2D> regionSegments;
     std::vector<HalfSegment2D> halfSegments;
 
-
-    void GetCyclicData();
+    void setFlags();
     SimplePoint2D GetDominatePoint(HalfSegment2D inputHalfSegment);
-    HalfSegment2D GetNextHalfSegCounterClock(HalfSegment2D currentHalfSeg);
     HalfSegment2D GetBrotherSeg(HalfSegment2D currentHalfSeg);
-    HalfSegment2D GetNextHalfSegClock(HalfSegment2D currentHalfSeg);
     bool GetAboveFlag(HalfSegment2D currentHalfSeg);
     //void setFlagsInCycle(std::vector<HalfSegment2D> cycle);
 };
@@ -51,14 +48,7 @@ Region2D::Impl::Impl(std::vector<Segment2D> _regionSegments)
 
 
     this->halfSegments = HalfSegVec;
-    try
-    {
-        GetCyclicData();
-    }
-    catch (...) {
-        std::cout << "Input Segments Don't Form a Valid Region" << std::endl;
-    }
-    sort(regionSegments.begin(), regionSegments.end());
+    setFlags();
 }
 
 SimplePoint2D Region2D::Impl::GetDominatePoint(HalfSegment2D inputHalfSegment)
@@ -90,35 +80,7 @@ bool Region2D::Impl::GetAboveFlag(HalfSegment2D currentHalfSeg)
     }
 }
 
-HalfSegment2D Region2D::Impl::GetNextHalfSegClock(HalfSegment2D currentHalfSeg)
-{
-    HalfSegment2D nextHalfSeg;
-    for(int i = halfSegments.size(); i >= 0; i--)
-    {
-        if(GetDominatePoint(currentHalfSeg) == GetDominatePoint(halfSegments[i]))
-        {
-            nextHalfSeg = halfSegments[i];
-            break;
-        }
-    }
-    return nextHalfSeg;
-}
-
-HalfSegment2D Region2D::Impl::GetNextHalfSegCounterClock(HalfSegment2D currentHalfSeg)
-{
-    HalfSegment2D nextHalfSeg;
-    for(int i = 0; i < halfSegments.size(); i++)
-    {
-        if(GetDominatePoint(currentHalfSeg) == GetDominatePoint(halfSegments[i]))
-        {
-            nextHalfSeg = halfSegments[i];
-            break;
-        }
-    }
-    return nextHalfSeg;
-}
-
-void Region2D::Impl::GetCyclicData()
+void Region2D::Impl::setFlags()
 {
     //initializing sweep structures
     std::queue<HalfSegment2D> sweepQueue;
@@ -128,127 +90,26 @@ void Region2D::Impl::GetCyclicData()
     {
         sweepQueue.push(halfSegments[i]);
     }
-    std::vector<HalfSegment2D> annotated; //will contain every half segment that has been visited/annotated
-    std::vector<SimplePoint2D> visitedDP; //will contain every visited dominate point
-    int walkAdjacency; //will be used to tell cycle walk which adjacency to use
 
     HalfSegment2D currentHalfSeg;
 
-    while(!sweepQueue.empty()) //while not end of sweep
+    while(!sweepQueue.empty())
     {
-        //moving current segment from queue to sweep status and adjusting accordingly
-        currentHalfSeg = sweepQueue.front();
-        if(currentHalfSeg.isDominatingPointLeft) //adding segment to sweep status
+        if(sweepStatus.empty())
         {
-            if(sweepStatus.empty())
-            {
-                sweepStatus.push_back(currentHalfSeg); //add half segment to sweep status at beginning
-                regionSegments.push_back(AttributedHalfSegment2D(currentHalfSeg,true));
-                regionSegments.push_back(AttributedHalfSegment2D(GetBrotherSeg(currentHalfSeg), true));
-            }
-            else if(GetDominatePoint(currentHalfSeg) == GetDominatePoint(sweepStatus.back()))
-            {
-                sweepStatus.push_back(currentHalfSeg);
-                regionSegments.push_back(AttributedHalfSegment2D(currentHalfSeg, !(GetAboveFlag(sweepStatus.back()))));
-                regionSegments.push_back(AttributedHalfSegment2D(GetBrotherSeg(currentHalfSeg), !(GetAboveFlag(sweepStatus.back()))));
-            }
-            else
-            {
-                int i = sweepStatus.size() - 1;
-                while(GetDominatePoint(currentHalfSeg).y < GetDominatePoint(sweepStatus[i]).y)
-                {
-                    i -= 1;
-                    if(i == -1) {
-                        break;
-                    }
-                }
-                while(GetDominatePoint(currentHalfSeg).y < GetDominatePoint(GetBrotherSeg(sweepStatus[i])).y)
-                {
-                    i -= 1;
-                    if(i == -1) {
-                        break;
-                    }
-                }
-                sweepStatus.emplace(sweepStatus.begin() + (i + 1), currentHalfSeg);
-                if(sweepStatus.front() == currentHalfSeg)
-                {
-                    regionSegments.push_back(AttributedHalfSegment2D(currentHalfSeg, true));
-                    regionSegments.push_back(AttributedHalfSegment2D(GetBrotherSeg(currentHalfSeg), true));
-                }
-                else
-                {
-                    regionSegments.push_back(AttributedHalfSegment2D(currentHalfSeg, !(GetAboveFlag(sweepStatus[i]))));
-                    regionSegments.push_back(AttributedHalfSegment2D(GetBrotherSeg(currentHalfSeg), !(GetAboveFlag(sweepStatus[i]))));
-                }
-            }
+            sweepStatus.push_back(currentHalfSeg); //add half segment to sweep status at beginning
+            regionSegments.push_back(AttributedHalfSegment2D(currentHalfSeg,true));
+            regionSegments.push_back(AttributedHalfSegment2D(GetBrotherSeg(currentHalfSeg), true));
         }
-        else { //removing element from sweep status
-            HalfSegment2D temp = HalfSegment2D(currentHalfSeg.s, true);
-            sweepStatus.erase(find(sweepStatus.begin(), sweepStatus.end(), temp)); //remove segment from sweep status once end of it is reached
-        }
-        sweepQueue.pop(); //removing current half segment from queue
-
-        if(find(annotated.begin(), annotated.end(), currentHalfSeg) != annotated.end()) //if segment is already visited continue
+        else if(GetDominatePoint(currentHalfSeg) == GetDominatePoint(sweepStatus.back())) 
         {
-            continue;
+            sweepStatus.push_back(currentHalfSeg);
+            regionSegments.push_back(AttributedHalfSegment2D(currentHalfSeg, !(GetAboveFlag(sweepStatus.back()))));
+            regionSegments.push_back(AttributedHalfSegment2D(GetBrotherSeg(currentHalfSeg), !(GetAboveFlag(sweepStatus.back()))));
         }
         else
         {
-            std::vector<HalfSegment2D> currentCycle; // will contain all half segments in current cycle
-            currentCycle.push_back(currentHalfSeg); //add currently unannotated half segment to new cycle
-            currentCycle.push_back(GetBrotherSeg(currentHalfSeg)); //add brother to current half segment to current cycle
-            visitedDP.push_back(GetDominatePoint(currentHalfSeg)); //add new segments DP to visited (Visitdp(c))
-            annotated.push_back(currentHalfSeg); // add current half segment to annotated
-            annotated.push_back(GetBrotherSeg(currentHalfSeg));  //add brother to annotated/visited
-
-            HalfSegment2D nextHalfSeg;
-            HalfSegment2D prevHalfSeg = sweepStatus[find(sweepStatus.begin(), sweepStatus.end(), currentHalfSeg) - sweepStatus.begin() - 1];
-            if(find(regionSegments.begin(), regionSegments.end(), AttributedHalfSegment2D(prevHalfSeg, true)) != regionSegments.end()) //if current half segment belongs to a hole
-            {
-                //getting h+ from c counterclockwise adjacency
-                nextHalfSeg = GetNextHalfSegCounterClock(GetBrotherSeg(currentHalfSeg));
-                walkAdjacency = 0;
-            }
-            else
-            {
-                //getting h+ from c clockwise adjacency
-                nextHalfSeg = GetNextHalfSegClock(GetBrotherSeg(currentHalfSeg));
-                walkAdjacency = 1;
-            }
-
-            while(!(currentHalfSeg == nextHalfSeg))
-            {
-
-                if(std::find(visitedDP.begin(), visitedDP.end(), GetDominatePoint(nextHalfSeg)) != visitedDP.end()) //if visitedDP(c)
-                {
-                    HalfSegment2D q = nextHalfSeg;
-                    nextHalfSeg = GetNextHalfSegCounterClock(nextHalfSeg);
-                    while(!(GetDominatePoint(nextHalfSeg) == GetDominatePoint(q))) //cycle backwards to anchor point (dp(q))
-                    {
-                        annotated.pop_back(); //remove current half segment from annotated list
-                        annotated.pop_back(); //remove current brother segment from annotated list
-                        nextHalfSeg = GetNextHalfSegCounterClock(nextHalfSeg);
-                    }
-                    nextHalfSeg = q;
-                }
-                else
-                {
-                    currentCycle.push_back(nextHalfSeg); //add current half seg to current cycle
-                    visitedDP.push_back(GetDominatePoint(nextHalfSeg)); //visit current DP
-                    HalfSegment2D brotherHalfSeg = GetBrotherSeg(nextHalfSeg); // get brother half seg
-                    currentCycle.push_back(brotherHalfSeg); // add brother to current cycle
-                    annotated.push_back(nextHalfSeg);
-                    annotated.push_back(brotherHalfSeg);
-                    if(walkAdjacency == 0) //counterclockwise adjacency
-                    {
-                        nextHalfSeg = GetNextHalfSegCounterClock(brotherHalfSeg);
-                    }
-                    else
-                    {
-                        nextHalfSeg = GetNextHalfSegClock(brotherHalfSeg);
-                    }
-                }
-            }
+            //use slope to find location in sweep status
         }
     }
 }
