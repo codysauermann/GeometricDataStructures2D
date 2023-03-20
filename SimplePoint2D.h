da37bd46ba7d093dc5c42d49450039f50ca9dab4
Oscar Rodas <60824325+thescar101@users.noreply.github.com>
2023-03-20 12:38:43 -0400

    Add  move assignment and changed copy assignment operators (SimplePoint2D, Segment2D, HalfSegment2D)
    Also added Line2D test to main

--- SimplePoint2D.h
+++ SimplePoint2D.h
@@ -9,7 +9,8 @@ struct SimplePoint2D
 	SimplePoint2D(const SimplePoint2D& p);
 	SimplePoint2D(Number x, Number y);
 	SimplePoint2D(SimplePoint2D&& p);
-	void operator=(const SimplePoint2D p);
+    SimplePoint2D& operator=(SimplePoint2D&& p);
+	SimplePoint2D& operator=(const SimplePoint2D& p);
 	bool operator<(const SimplePoint2D p);
 	bool operator<=(const SimplePoint2D p);
 	bool operator==(const SimplePoint2D p);
