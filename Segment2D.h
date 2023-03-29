f1f0a15969c045dcf9b9d19556853e51de2a9f60
shudgit <shudouglas@ufl.edu>
2023-03-29 02:05:56 -0400

    Made findIntersection also return a bool flag.

--- Segment2D.h
+++ Segment2D.h
@@ -16,6 +16,6 @@ struct Segment2D
 	bool operator>=(const Segment2D& s);
 	bool operator>(const Segment2D& s);
 	bool operator!=(const Segment2D& s);
-	SimplePoint2D findIntersection(Segment2D s);
+	std::pair<bool, SimplePoint2D> findIntersection(Segment2D s);
 };
 #endif
